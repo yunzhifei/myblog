e04581199b32539a5660e9c250fe30f819c4c6b2
yunzhifei <yunzhifei_chd@139.com>
2017-07-28 22:06:02 +0800

    add origin

---
+++ src/main/java/bj/my/blog/blog/start/Application.java
@@ -0,0 +1,12 @@
+package bj.my.blog.blog.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}

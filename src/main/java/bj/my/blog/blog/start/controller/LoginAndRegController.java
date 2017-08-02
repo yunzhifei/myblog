17282d03242d1b04d4d998199cbbcc738f53380a
yunzhifei <yunzhifei_chd@139.com>
2017-08-02 15:56:26 +0800

    test Template and red line

--- src/main/java/bj/my/blog/blog/start/controller/LoginAndRegController.java
+++ src/main/java/bj/my/blog/blog/start/controller/LoginAndRegController.java
@@ -1,6 +1,8 @@
 package bj.my.blog.blog.start.controller;
 
+import bj.my.blog.blog.start.util.DESUtil;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,11 +10,13 @@ public class LoginAndRegController {
     @RequestMapping("/registerBefore")
     public String registerBefore() {
         //返回一个注册页面模板
+
         return "";
     }
-    @RequestMapping("register")
-    public String register(){
-        //注册并返回到首页已登录状态想
-        return "redirect:/";
+    @RequestMapping("/register")
+    public String register(Model model){
+        //注册并返回到首页已登录状态
+        model.addAttribute("DES",new DESUtil());
+        return "register";
     }
 }

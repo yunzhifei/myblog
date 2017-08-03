f744b883b5b6f8d2f0b7c1bc1bc00df56e57866c
yunzhifei <yunzhifei_chd@139.com>
2017-08-03 15:25:07 +0800

    add redis config and test

--- src/test/java/bj/my/blog/blog/start/ApplicationTests.java
+++ src/test/java/bj/my/blog/blog/start/ApplicationTests.java
@@ -1,11 +1,16 @@
 package bj.my.blog.blog.start;
 
+import bj.my.blog.blog.start.config.RedisConfig;
 import bj.my.blog.blog.start.dao.IUserDao;
 import bj.my.blog.blog.start.model.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -13,21 +18,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class ApplicationTests {
     @Autowired
     IUserDao userDao;
+    @Autowired
+    RedisTemplate<?, ?> redisTemplate;
 
     @Test
     public void contextLoads() {
-        User user = new User();
-        user.setName("liu");
-        user.setPassword("cao");
-        user.setSalt("nimabi");
-        user.setPhone("234567896");
-        user.setUserId(6);
-        System.out.println("user = " + user);
 
-        long s = userDao.addUser(user);
-        System.out.println("s = " + s);
+        redisTemplate.execute(new RedisCallback<Boolean>() {
+            @Override
+            public Boolean doInRedis(RedisConnection redisConnection) throws DataAccessException {
+                redisConnection.set("w".getBytes(), "g".getBytes());
+                return true;
+            }
+        });
 
-        System.out.println("user = " + user);
     }
 
 }

b52127b97943833c9c65f0ec7b54e5478f0839b4
yunzhifei <yunzhifei_chd@139.com>
2017-08-14 14:54:15 +0800

    update send mail

--- src/main/java/bj/my/blog/blog/start/service/MailService.java
+++ src/main/java/bj/my/blog/blog/start/service/MailService.java
@@ -4,9 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.Map;
 
 @Service
 public class MailService {
@@ -25,10 +34,47 @@ public class MailService {
         logger.info(from);
         try {
             mailSender.send(message);
-
             logger.info("邮件已经发送！");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendHtmlMail(String to, String subject, String content, String[] filepaths) {
+        MimeMessage message = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setFrom(from);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(content, true);
+            //如果有附件就添加附件
+            for (String filePath : filepaths) {
+                FileSystemResource fileSystemResource = new FileSystemResource(new File(filePath));
+                String fileName = filePath.substring(filePath.lastIndexOf(File.separatorChar));
+                mimeMessageHelper.addAttachment(fileName, fileSystemResource);
+            }
+            mailSender.send(message);
+            logger.info("html 发送成功！");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendTemplateMail(String to, String subject, String template, Map<String, Object> params, String[] filePaths) {
+        Context context = new Context();
+        //设置传入的参数值
+        for (String key : params.keySet()) {
+            context.setVariable(key, params.get(key));
+        }
+        //初始化模板引擎
+        TemplateEngine templateEngine = new TemplateEngine();
+        String content = templateEngine.process(template, context);
+        sendHtmlMail(to, subject, content, filePaths);
+    }
+
+    //发送带附件的html模板文件
+    public void sendAttachMail(String to, String subject, String template, Map<String, Object> params, String[] filePaths) {
+        sendTemplateMail(to, template, subject, params, filePaths);
     }
 }
